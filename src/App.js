3426ae24d93815194361dff978729b6672333480
Feline Huhn <feline.huhn@gmx.de>
2023-09-20 14:08:25 +0200

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
+  function handleDecreaseCount() {
+    setCount(count - 1);
+  }
+
+  function handleIncreaseCount() {
+    setCount(count + 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleDecreaseCount}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleIncreaseCount}>
           +
         </button>
       </div>
